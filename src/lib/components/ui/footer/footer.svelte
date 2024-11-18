114444c55c15e5c22d4b037962767926cbb8bbd8
Tormod Liseth <69258630+sifterstudios@users.noreply.github.com>
2024-11-18 07:57:44 +0100

    change darkmode button to secondary

--- src/lib/components/ui/footer/footer.svelte
+++ src/lib/components/ui/footer/footer.svelte
@@ -14,7 +14,7 @@
     </div>
 
     <ModeWatcher />
-    <Button on:click={toggleMode} size="icon">
+    <Button variant="secondary" on:click={toggleMode} size="icon">
       <Sun
         class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
       />
